9b13023116180f964e67eceb8f5c07b62480ff1f
Shanna Jones <shanna@jonesbunch.com>
2025-01-03 14:55:33 +0100

    Search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#cityName");
+
+  cityElement.innerHTML = response.data.city;
+  //Moved this from the updateCity function so that the city spelling and format would be the result of the API call.
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function updateCity(event) {
   event.preventDefault();
   let citySearch = document.querySelector("#searchInput");
-  let cityElement = document.querySelector("#cityName");
-  cityElement.innerHTML = citySearch.value;
+  searchCity(citySearch.value);
 }
 
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", updateCity);
+
+searchCity("Tübingen");
