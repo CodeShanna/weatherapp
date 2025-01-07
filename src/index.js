3c12c23677e4da267d14649368bf3c6f1dcaeeee
Shanna Jones <shanna@jonesbunch.com>
2025-01-07 14:56:12 +0100

    Inject 5 day forecast from JS

--- src/index.js
+++ src/index.js
@@ -50,8 +50,29 @@ function updateCity(event) {
   let citySearch = document.querySelector("#searchInput");
   searchCity(citySearch.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
 
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="forecast-day">
+          <div class="forecast-date">${day}</div>
+          <div class="forecast-icon">☀️</div>
+          <div class="forecast-temperatures">
+              <div class="forecast-temperature"><strong>15°</strong></div>
+              <div class="forecast-temperature">9°</div>
+          </div>
+        </div>
+      `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", updateCity);
 
 searchCity("Tübingen");
+displayForecast();
